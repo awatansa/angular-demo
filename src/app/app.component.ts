55f3e872ea45495272b950a7ab03b39964b60d4b
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2024-03-03 03:02:34 +0530

    Added basic components
    - home page
    - root layout page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { RootLayoutComponent } from './shared/components/root-layout/root-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, RootLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
